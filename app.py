a9529e5b5ac078c1fdd2be9bd376a2e11540b11d
Tran <127738056+vinetran@users.noreply.github.com>
2023-05-30 23:18:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ dataset = st.container()
 recommendation = st.container()
 
 with header:
-    header_ = '<p style="font-family:sans-serif; color:DarkGreen; font-size: 60px;">DEEP MOVIE RECOMMENDATION</p>'
+    header_ = '<p style="font-family:sans-serif; color:DarkGreen; font-size: 46px;">DEEP MOVIE RECOMMENDATION</p>'
     st.markdown(header_, unsafe_allow_html=True)
     text0 = '<p style="font-family:Courier; color:midnightblue; font-size: 24px;">With this application, we would like to make a list of movies that you will love watching!</p>'
     st.markdown(text0, unsafe_allow_html=True)
     #st.text('With this application, we would like to make a list of movies that you will love watching!')
 
 with dataset:
-    st.header('WE RECOMMEND MOVIES FROM')
-    col1, col2, col3 = dataset.columns(3)
+    st.header('WE BRING TO YOU THE BEST MOVIE COLLECTION FROM')
+    col1, col2, col3, col4, col5 = dataset.columns(5)
     logo_netflix = Image.open('source/image_streamlit/netflix.jpg')
     logo_amazon_prime = Image.open('source/image_streamlit/primevideo.png')
     logo_apple_tv = Image.open('source/image_streamlit/apple_tv.png')
@@ -28,10 +28,10 @@ with dataset:
     with col1:
         st.image(logo_netflix)
         st.image(logo_hbo)
-    with col2:
+    with col3:
         st.image(logo_amazon_prime)
         st.image(logo_apple_tv)
-    with col3:
+    with col5:
         st.image(logo_paramount)
         st.image(logo_disney)
 
