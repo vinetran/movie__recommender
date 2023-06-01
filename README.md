90da90526f8c089624ea0b75c84462b12f935658
vinetran <qv_tran@yahoo.com>
2023-06-01 19:06:40 +0200

    update Readme / app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## Building Movie Recommender Project 
+
+### The project aims to build a movie recommender based on movie attributes (title, genre, age certification, production country, actors, imdb score, imdb number of votes, tmdb popularity, tmdb score, duration and description/summary)
+
+### [Project data can be retrieved here] (https://www.kaggle.com/datasets/dgoenrique/netflix-movies-and-tv-shows). The data contains over 19,000 movies titles collected from different movie platforms: Netflix, Amazon prime movies, AppleTV, Disney+, HBO max and Paramount.
+
+### Recommendation is based on the similarity between movies which consists of 2 parts: similarity of the summaries and that of the remaining characteristics. For the first one, an autoencoders model was implemented. Cosine similarity was called for calculating similarites. The final similarity matrix was recuperated by taking average of these 2 similarity scores.
+
 [The application can be reached here] (https://vinetran-movie--recommender-app-tn7gmf.streamlit.app/)
+
--- app.py
+++ app.py
@@ -10,13 +10,13 @@ dataset = st.container()
 recommendation = st.container()
 
 with header:
-    header_ = '<p style="font-family:sans-serif; color:DarkGreen; font-size: 46px;">MOVIE RECOMMENDATION</p>'
+    header_ = '<p style="font-family:sans-serif; color:darkcyan; font-size: 46px;">MOVIE RECOMMENDATION</p>'
     st.markdown(header_, unsafe_allow_html=True)
-    text0 = '<p style="font-family:Courier; color:midnightblue; font-size: 24px;">With this application, we would like to make a list of movies that you will love watching!</p>'
+    text0 = '<p style="font-family:Courier; color:firebrick; font-size: 24px;">With this application, we would like to make a list of movies that you will love watching!</p>'
     st.markdown(text0, unsafe_allow_html=True)
     
 with dataset:
-    st.header('WE BRING TO YOU THE BEST MOVIE COLLECTION FROM')
+    st.header('WE BRING TO YOU THE BEST MOVIE COLLECTION')
     col1, col2, col3 , col4, col5 = dataset.columns(5)
     logo_netflix = Image.open('source/image_streamlit/netflix.jpg')
     logo_amazon_prime = Image.open('source/image_streamlit/primevideo.png')
@@ -38,14 +38,15 @@ with recommendation:
     st.header('LET US MAKE RECOMMENDATION TO YOU')
     text1 = '<p style="font-family:Courier; color:darkcyan; font-size: 20px;">Now you show us one of your favorite movies ...</p>'
     st.markdown(text1, unsafe_allow_html=True)
-    text2 = '<p style="font-family:Courier; color:darkcyan; font-size: 20px;">... then let we recommend you movies that you will love</p>'
+    text2 = '<p style="font-family:Courier; color:darkcyan; font-size: 20px;">... then let we recommend you movies that you will love!</p>'
     st.markdown(text2, unsafe_allow_html=True)
       
     frame = joblib.load('source/model/pickle_frame.joblib')
-    movie_chosen = st.selectbox('SELECT or TYPE here below the movie you loved watching:', options=[title for title in frame.columns.sort_values()])
-    st.write('Your favourite movie chosen:', movie_chosen)
-   
-    st.subheader('Here are movies that we recommend you:')
+    st.write('### **SELECT** or **TYPE** here below the movie title you loved watching:')
+    movie_chosen = st.selectbox('', options=[title for title in frame.columns.sort_values()])
+    st.write('### **Your favourite movie chosen:**')
+    st.subheader(movie_chosen)
+    st.write('### **Here are movies that we recommend you:**', unsafe_allow_html=True)
     recommendation_list = list(frame[movie_chosen].sort_values(ascending=False)[1:11].index.values)
     title = ''
     for i in recommendation_list:
